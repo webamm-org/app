78d4c9e3118fea3fe7867e327fa5cbd9a516b813
Paweł Dąbrowski <dziamber@gmail.com>
2024-08-23 17:33:35 +0200

    Remove the front page and redirect to account

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   devise_for :users
-  root 'home#index'
+  root 'plans#index'
 
   namespace :api do
     namespace :v1 do
